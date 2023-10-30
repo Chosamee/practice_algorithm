72c8bc40a9ea56a9030b38e3c132ec4a312a3e85
Lee-search <cytor@naver.com>
2023-10-30 11:03:22 +0900

    backup

--- etc_boj/Main.java
+++ etc_boj/Main.java
@@ -4,16 +4,9 @@ import java.io.InputStreamReader;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        boolean[] arr = new boolean[31];
-
-        for (int i = 0; i < 28; i++) {
-            arr[Integer.parseInt(br.readLine())] = true;
-        }
-        for (int i = 1; i < 31; i++) {
-            if (!arr[i])
-                sb.append(i).append('\n');
-        }
-        System.out.println(sb);
+        String S, P;
+        S = br.readLine();
+        P = br.readLine();
+        System.out.println(S.contains(P) ? 1 : 0);
     }
 }
