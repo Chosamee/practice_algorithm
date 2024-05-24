aad24ef222693ccf1273d3322c27bc7ac8aebfd4
Chosamee <cytor@naver.com>
2024-05-24 17:58:21 +0900

    BJ1858 푸는중

--- etc_boj/BJ1858.java
+++ etc_boj/BJ1858.java
@@ -12,14 +12,17 @@ class Xy implements Comparable<Xy> {
         this.x = x;
         this.y = y;
         // x==0일때??
-        this.w = Math.abs((y - x) * y / x);
+        // this.w = Math.abs((y - x) * y / x);
     }
 
     @Override
     public int compareTo(Xy o) {
-        if (this.w == o.w)
-            return this.n - o.n;
-        return this.w - o.w;
+        return this.x - o.x;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d %d", n, x, y);
     }
 }
 
@@ -27,10 +30,9 @@ public class BJ1858 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static Xy[] nodes;
-    static int N;
 
     static void init() throws Exception {
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         nodes = new Xy[N];
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
@@ -40,10 +42,31 @@ public class BJ1858 {
         }
     }
 
+    public static int solution(Xy[] nodes) {
+        // System.out.println(Arrays.toString(nodes));
+
+        int max = -1;
+        int maxIdx = 0;
+        for (int i = 0; i < nodes.length - 1; i++) {
+            int incline = Math.abs((nodes[i + 1].y - nodes[i].y) / (nodes[i + 1].x - nodes[i].x));
+            if (incline == max && (nodes[i].n < nodes[maxIdx].n || nodes[i + 1].n < nodes[maxIdx].n)) {
+                maxIdx = i;
+            } else if (incline > max) {
+                max = incline;
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+
     public static void main(String[] args) throws Exception {
         init();
         Arrays.sort(nodes);
-        System.out.printf("%d %d", nodes[0].n, nodes[N - 1].n);
+        int idx = solution(nodes);
+        if (nodes[idx].n < nodes[idx + 1].n)
+            System.out.printf("%d %d", nodes[idx].n, nodes[idx + 1].n);
+        else
+            System.out.printf("%d %d", nodes[idx + 1].n, nodes[idx].n);
     }
 
 }
